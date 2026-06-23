8e5286144dda8adda941ad33aae0ddee2e6eba91
Naresh S <nareshsp963@gmail.com>
2026-06-23 16:56:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello Azure DevOps CI/CD"
+    return "Hello Azure DevOps CI/CD updated"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
-    
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000))
+    )
